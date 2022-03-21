c031bd6737cd3c1daf3276a69565c58f707b6df6
damart93 <38332576+damart93@users.noreply.github.com>
2022-03-21 11:18:31 +0100

    Update db.py

--- tap_oracle/db.py
+++ tap_oracle/db.py
@@ -10,6 +10,7 @@ def make_dsn(config):
    return cx_Oracle.makedsn(config["host"], config["port"], service_name=config["sid"])
 
 def open_connection(config):
-    LOGGER.info("dsn: %s", make_dsn(config))
-    conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
+    LOGGER.info("dsn: %s", config["connection_string"])
+    #conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
+    conn = cx_Oracle.connect(user="hr", password=userpwd, dsn=config["connection_string"])
     return conn
