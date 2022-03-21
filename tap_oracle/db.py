fe706a0e0e2ed650d5d4d7a2a421329783af3233
damart93 <38332576+damart93@users.noreply.github.com>
2022-03-21 11:35:04 +0100

    Update db.py

--- tap_oracle/db.py
+++ tap_oracle/db.py
@@ -6,14 +6,12 @@ LOGGER = singer.get_logger()
 def fully_qualified_column_name(schema, table, column):
     return '"{}"."{}"."{}"'.format(schema, table, column)
 
+'''
 def make_dsn(config):
    return cx_Oracle.makedsn(config["host"], config["port"], service_name=config["sid"])
+'''
 
 def open_connection(config):
     LOGGER.info("dsn: %s", config["connection_string"])
-    #conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
-    if "connection_string" in config:
-        conn = cx_Oracle.connect(user=config["user"], password=config["password"], dsn=config["connection_string"])
-    else:
-        conn = cx_Oracle.connect(config["user"], config["password"], make_dsn(config))
+    conn = cx_Oracle.connect(user=config["user"], password=config["password"], dsn=config["connection_string"])
     return conn
